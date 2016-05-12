0bbf47f588db42c3e700afe4afb0ee86e9829bde
Jackson Tong <tongtoan2704@gmail.com>
2016-05-12 22:50:51 +0700

    intial

---
+++ spec/Factorial_spec.js
@@ -0,0 +1,8 @@
+describe("Factorial", function () {
+    it("should get factorial of given number", function () {
+
+    });
+    it("Should return null value for passing negative number of less/more than one argument", function () {
+        
+    });
+});
