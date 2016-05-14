339ccacdec06cf37bebb8bb48cbcf1a4d79ac50e
Jackson Tong <tongtoan2704@gmail.com>
2016-05-14 23:13:11 +0700

    setup and teardown

--- spec/InventoryStoc_spec.js
+++ spec/InventoryStoc_spec.js
@@ -1,26 +1,23 @@
 describe('Inventory Stock', function () {
+    beforeEach(function () {
+        stockinhand_item1 = 11;
+        item1 = 1;
+    });
+    afterEach(function () {
+        stockinhand_item1 = 0;
+        item1 = 0;
+    });
     it('Inventory Stock should be updated on sale/issue of an item', function () {
-        var stockinhand_item1 = 11;
-        var item1 = 1;
-        var transaction = 'SALE';
         expect(stockinhand_item1 - item1).toEqual(10);
     });
     it('Inventory Stock should be updated on issue of an item within organization', function () {
-        var stockinhand_item1 = 11;
-        var item1 = 1;
         var transaction = 'ISSUE';
         expect(stockinhand_item1 - item1).toEqual(10);
     });
     it('Inventory Stock should be updated on return of any item', function () {
-        var stockinhand_item1 = 11;
-        var item1 = 1;
-        var transaction = 'SALE RETURN';
         expect(stockinhand_item1 + item1).toEqual(12);
     });
     it('Inventory Stock should be updated on receiving or procuring new item', function () {
-        var stockinhand_item1 = 11;
-        var item1 = 1;
-        var transaction = 'PROCUREMENT';
         expect(stockinhand_item1 + item1).toEqual(12);
     });
 });
