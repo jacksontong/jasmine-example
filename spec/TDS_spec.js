5c52b13a078c34ecdb2726b05fc9339c9b6f3001
Jackson Tong <tongtoan2704@gmail.com>
2016-05-16 23:08:47 +0700

    refactor spec

--- spec/TDS_spec.js
+++ spec/TDS_spec.js
@@ -1,40 +1,68 @@
 describe("Employees of <XYZ> Company:", function () {
-    describe('Tax deducted for Indian Employees, ', function () {
+    describe('TAX/TDS Currency', function () {
+        var index = 0;
+        var myRegion, myCurrency;
         beforeEach(function () {
-            this.myTaxableIncome = new TaxIndiaEmp();
+            myRegion = ['INDIA', 'UK', 'US'];
+            myCurrency = new Currency(myRegion[index]);
         });
         afterEach(function () {
-            this.myTaxableIncome = undefined;
+            index += 1;
         });
+        //Scenario -1
         it('Currency should be used INR', function () {
-            var myCurrency = new Currency('INDIA');
             expect(myCurrency.currency).toBe('INR');
         });
-        it('Should be duducted 10% if Gross Income is between RS 250,000/- AND RS 500,000/-', function () {
-            //Let's assume the taxable income is RS 300,000/-
-            this.myTaxableIncome.setIncome(300000);
-            expect(this.myTaxableIncome.calculateTDS()).toEqual(5000);
-        });
-        it('Should be deducted 20% if Gross Income is between RS 500,000/- and RS 1,000,000/-', function () {
-            //Let's assume the taxable income is RS 700,000/-
-            this.myTaxableIncome.setIncome(700000);
-            expect(this.myTaxableIncome.calculateTDS()).toEqual(40000);
-        });
-        it('Should be deducted 30% if Gross Income is >RS 10,000,00/-', function () {
-            this.myTaxableIncome.setIncome(1300000);
-            expect(this.myTaxableIncome.calculateTDS()).toEqual(90000);
-        });
-    });
-    describe('Tax deducted for United Kingdom Employees', function () {
+        //Scenario -2
         it('Currency should be used GBP (Pound, UK£)', function () {
-            var myCurrency = new Currency('UK');
             expect(myCurrency.currency).toBe('UK£');
         });
-    });
-    describe('Tax deducted for United States Employees', function () {
+        //Scenario -3
         it('Currency should be used USD (US$)', function () {
-            var myCurrency = new Currency('US');
             expect(myCurrency.currency).toBe('US$');
+        });
+    });
+    describe('TAX/TDS', function () {
+        describe('Tax deducted for Indian Employees, ', function () {
+            var index = 0;
+            var grossTaxableIncome = [300000, 700000, 1300000];
+            var myTaxableIncome;
+            beforeEach(function () {
+                myTaxableIncome = new TaxIndiaEmp();
+                myTaxableIncome.setIncome(grossTaxableIncome[index]);
+            });
+            afterEach(function () {
+                index += 1;
+            });
+
+            //Scenario -4
+            it('Should be duducted 10% if Gross Income is between RS 250,000/- AND RS 500,000/-', function () {
+                //Let's assume the taxable income is RS 300,000/-
+                expect(myTaxableIncome.calculateTDS()).toEqual(5000);
+            });
+            //Scenario -5
+            it('Should be deducted 20% if Gross Income is between RS 500,000/- and RS 1,000,000/-', function () {
+                //Let's assume the taxable income is RS 700,000/-
+                expect(myTaxableIncome.calculateTDS()).toEqual(40000);
+            });
+            //Scenario -6
+            it('Should be deducted 30% if Gross Income is >RS 10,000,00/-', function () {
+                expect(myTaxableIncome.calculateTDS()).toEqual(90000);
+            });
+        });
+        describe('Tax deducted for United Kingdom Employees', function () {
+
+            xit('Should be deducted 20% if Gross Annual Income is between £10,000/- and £32,000', function () {
+
+            });
+        });
+        describe('Tax deducted for United States Employees', function () {
+            xit('Should be deducted 35% of Gross Taxable Income if employee status is SINGLE', function () {
+
+            });
+            xit('Should be deducted 30% of Gross Taxable Income if employee status is MARRIED', function () {
+
+            });
         });
     });
 });
