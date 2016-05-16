18f66ecaf21c351be371fc5c7b08bd11663526e0
Jackson Tong <tongtoan2704@gmail.com>
2016-05-16 22:49:14 +0700

    refactor tax for Indian

--- spec/TDS_spec.js
+++ spec/TDS_spec.js
@@ -1,15 +1,29 @@
 describe("Employees of <XYZ> Company:", function () {
     describe('Tax deducted for Indian Employees, ', function () {
+        beforeEach(function () {
+            this.myTaxableIncome = new TaxIndiaEmp();
+        });
+        afterEach(function () {
+            this.myTaxableIncome = undefined;
+        });
         it('Currency should be used INR', function () {
             var myCurrency = new Currency('INDIA');
             expect(myCurrency.currency).toBe('INR');
         });
-    });
-    it('Should be duducted 10% if Gross Income is between RS 250,000/- AND RS 500,000/-', function () {
-        var myTaxableIncome = new TaxIndiaEmp();
-        //Let's assume the taxable income is RS 300,000/-
-        myTaxableIncome.setIncome(300000);
-        expect(myTaxableIncome.calculateTDS()).toEqual(5000);
+        it('Should be duducted 10% if Gross Income is between RS 250,000/- AND RS 500,000/-', function () {
+            //Let's assume the taxable income is RS 300,000/-
+            this.myTaxableIncome.setIncome(300000);
+            expect(this.myTaxableIncome.calculateTDS()).toEqual(5000);
+        });
+        it('Should be deducted 20% if Gross Income is between RS 500,000/- and RS 1,000,000/-', function () {
+            //Let's assume the taxable income is RS 700,000/-
+            this.myTaxableIncome.setIncome(700000);
+            expect(this.myTaxableIncome.calculateTDS()).toEqual(40000);
+        });
+        it('Should be deducted 30% if Gross Income is >RS 10,000,00/-', function () {
+            this.myTaxableIncome.setIncome(1300000);
+            expect(this.myTaxableIncome.calculateTDS()).toEqual(90000);
+        });
     });
     describe('Tax deducted for United Kingdom Employees', function () {
         it('Currency should be used GBP (Pound, UK£)', function () {
--- src/TaxIndiaEmp.js
+++ src/TaxIndiaEmp.js
@@ -12,6 +12,10 @@ TaxIndiaEmp.prototype.calculateTDS = function () {
     var myTax = 0;
     if (this.getIncome() > 250000 && this.getIncome() <= 500000) {
         myTax = (this.getIncome() - 250000) * 10/100;
+    } else if (this.getIncome() > 500000 && this.getIncome() <= 1000000) {
+        myTax = (this.getIncome() - 500000) * 20/100;
+    } else if (this.getIncome() > 1000000) {
+        myTax = (this.getIncome() - 1000000) * 30/100;
     }
 
     return myTax;
