b855e4b92179827d62be4f49766fe3da72c79f4e
Jackson Tong <tongtoan2704@gmail.com>
2016-05-20 23:53:29 +0700

    creating specs

---
+++ spec/CurrencyConverter_spec.js
@@ -0,0 +1,31 @@
+describe('<ABC> Money Exchange Company: Currency Converter Module', function () {
+    describe('When Convert Currency Across Region: ', function () {
+        xit('Verify that Indian Rupees (INR) converted to Us Dollars (USD)', function () {
+
+        });
+        xit('Verify that Indian Rupees (INR) converted to Japanese Yen (JPY)', function () {
+
+        });
+        xit('Verify that Hong Kong Dollars (HKD) converted to Us Dollars (USD)', function () {
+
+        });
+        xit('Verify that Japanese Yen (JPY) converted to Us Dollars (USD)', function () {
+
+        });
+        xit('Verify that UAE Dirham (AED) converted to Us Dollars (USD)', function () {
+
+        });
+        xit('Verify that British Pound Sterling converted to Us Dollars (USD)', function () {
+
+        });
+        xit('Verify that South African Rand (ZAR) converted to Us Dollars (USD)', function () {
+
+        });
+        xit('Verify that Us Dollars (USD) converted to Hong Kong Dollars (HKD)', function () {
+
+        });
+        xit('Verify that Us Dollars (USD) converted to Japanese Yen (JPY)', function () {
+
+        });
+    });
+});
