15ade063e030692df1a0d0073d4186b35f8acbbb
Jackson Tong <tongtoan2704@gmail.com>
2016-05-21 14:03:10 +0700

    refactor

--- spec/CurrencyConverter_spec.js
+++ spec/CurrencyConverter_spec.js
@@ -37,4 +37,15 @@ describe('<ABC> Money Exchange Company: Currency Converter Module', function () 
             expect(myCurrency.convertedCurrency()).toEqual(112.81);
         });
     });
+    //Acceptance Criteria
+    describe('When Convert the Currency Across Asian Countries: ', function () {
+        it('Japanese Yen (JPY) should be converted to Indian Rupee (INR) with user defined exchange rate', function () {
+            var myCurrency = new CurrencyConverter(1, 'JPY', 'INR', 0.5500);
+            expect(myCurrency.convertedCurrency()).toEqual(0.5500);
+        });
+        it('Hong Kong Dollar (HKD) should be converted to Indian Rupee (INR) with user defined exchange rate', function () {
+            var myCurrency = new CurrencyConverter(10000, 'HKD', 'INR', 9.0);
+            expect(myCurrency.convertedCurrency()).toEqual(90000);
+        });
+    });
 });
